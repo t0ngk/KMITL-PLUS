e486558f77700d72978c056079e6c3216b449966
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2023-06-20 04:26:48 +0700

    :bug:  : Font doesn't load

--- src/content/studyTable.js
+++ src/content/studyTable.js
@@ -1,12 +1,25 @@
 import studyTable from "../lib/components/studyTable.svelte";
-import { scrapeTable, flattenStudyTable, sortByDay, getinfo } from "../lib/util/studyTable";
+import {
+  scrapeTable,
+  flattenStudyTable,
+  sortByDay,
+  getinfo,
+} from "../lib/util/studyTable";
 
-import '../assets/styles.css';
+import "../assets/styles.css";
 
 const getStudyTable = document.querySelector("table");
 const info = getinfo(getStudyTable);
 let scrapedData = sortByDay(flattenStudyTable(scrapeTable(getStudyTable)));
 const oldBody = document.body.innerHTML;
+
+// Add font เพราะเรียกจาก CSS แล้วมันจะไม่ load ให้
+const fontPrompt = document.createElement("style");
+fontPrompt.innerHTML = `
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap');
+`;
+document.head.appendChild(fontPrompt);
+
 document.body.innerHTML = "";
 new studyTable({
   target: document.body,
