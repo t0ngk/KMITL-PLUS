43819dc2d2920fc26e31ef53f63b0f0c95de6db5
T0NG.K <39180519+t0ngk@users.noreply.github.com>
2024-08-09 00:38:50 +0700

    🩹 : Remove space

--- src/content/examSchedule.ts
+++ src/content/examSchedule.ts
@@ -5,8 +5,8 @@ const examSchedule: HTMLTableElement | null = document.querySelector(
 	'body > center > form > table > tbody > tr:nth-child(5) > td > table'
 );
 if (examSchedule) {
-	const examData =  scrapData(examSchedule);
-  console.log(examData);
+	const examData = scrapData(examSchedule);
+	console.log(examData);
 } else {
 	console.error('[KMITL +] : No exam schedule found');
 }
